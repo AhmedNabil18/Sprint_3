17a5232c4b7e8feeb0087a36cadfe3b3916cfbea
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-07-21 16:20:41 +0200

    Add Spi Task S_AT_MC_06

---
+++ S_AT_MC_06/SPI Slave/SPI Slave/Debug/main.d
@@ -0,0 +1,21 @@
+main.d main.o: .././main.c .././MCAL/Spi\ Module/Spi.h \
+ .././MCAL/Spi\ Module/Spi_Cfg.h \
+ .././MCAL/Spi\ Module/..\Platform_Types.h \
+ .././MCAL/Spi\ Module/Spi_Symbols.h \
+ .././MCAL/Spi\ Module/..\ATMega32_Interrupts.h \
+ .././MCAL/Spi\ Module/..\ATMega32_Registers.h \
+ .././MCAL/Spi\ Module/..\Platform_Types.h
+
+.././MCAL/Spi\ Module/Spi.h:
+
+.././MCAL/Spi\ Module/Spi_Cfg.h:
+
+.././MCAL/Spi\ Module/..\Platform_Types.h:
+
+.././MCAL/Spi\ Module/Spi_Symbols.h:
+
+.././MCAL/Spi\ Module/..\ATMega32_Interrupts.h:
+
+.././MCAL/Spi\ Module/..\ATMega32_Registers.h:
+
+.././MCAL/Spi\ Module/..\Platform_Types.h:
